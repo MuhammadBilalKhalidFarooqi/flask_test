e4c2deabdf22c7db47a14c3c926f79f8f0d2cf60
bilal khan jan <bilal.khalid.bsai-2024a@cecosian.edu.pk>
2026-06-04 03:04:25 +0500

    added a try except block here

--- test_flask.py
+++ test_flask.py
@@ -8,12 +8,19 @@ def homepage():
 
 @app.route('/user_login', methods = ['POST', 'GET'])
 def my_login():
+    my_client_data = request.get_json()
     if request.method == 'POST':
-        my_client_data = request.get_json()
-        return jsonify({'status': 'Mission Successful', 'got this:' : my_client_data, 'request': 'POST'})
+        try:
+            result = int(my_client_data) + 100
+            return jsonify({'status': 'Mission Successful', 'got this:' : result, 'request': 'POST'})
+        except:
+            return 'big big error bara wala error aya tha!!'
         
     elif request.method == 'GET':
-        return jsonify({'status': 'Mission Successful', 'take this:' : "|PEACE AND LOVE|", 'request': 'GET'})
+        try:
+            return jsonify({'status': 'Mission Successful', 'take this' : my_client_data, 'request': 'GET'})
+        except :
+            return 'an error, I am sorry for it!!'
 
 
 @app.route('/<usr>')
@@ -23,4 +30,4 @@ def my_user(usr):
 
 if __name__ == "__main__":
 
-   app.run(port=9574, host='192.168.1.15')
+   app.run(port=9574, host='192.168.1.15', debug=True)
